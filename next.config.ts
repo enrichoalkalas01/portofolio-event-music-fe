28b86e9bb54ab5ea1b5681a60016078727c37e6c
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2026-02-08 14:59:37 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
     images: {
+        unoptimized: true,
         domains: [
             "storage.googleapis.com",
             "swarnatactical.com",
